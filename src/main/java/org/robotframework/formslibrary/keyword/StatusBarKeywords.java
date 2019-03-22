b958dea11debb7fc12908de43ebf6b5f8068c9c0
Henrik Vesterinen <henrik.vesterinen@siili.com>
2019-03-22 10:25:14 +0200

    StatusBarKeywords: add getAllStatusMessages
    * This returns all status messages from all status bars,
      not just one message from one status bar.
    
    * Adds corresponding Operator methods.
    
    Signed-off-by: Henrik Vesterinen <henrik.vesterinen@siili.com>

--- src/main/java/org/robotframework/formslibrary/keyword/StatusBarKeywords.java
+++ src/main/java/org/robotframework/formslibrary/keyword/StatusBarKeywords.java
@@ -1,5 +1,8 @@
 package org.robotframework.formslibrary.keyword;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.robotframework.formslibrary.operator.StatusBarOperator;
 import org.robotframework.javalib.annotation.ArgumentNames;
 import org.robotframework.javalib.annotation.RobotKeyword;
@@ -8,9 +11,28 @@ import org.robotframework.javalib.annotation.RobotKeywords;
 @RobotKeywords
 public class StatusBarKeywords {
 
-    @RobotKeyword("Get the message that is displayed in the status bar at the bottom of the screen. \n\n Example:\n | Get Status Message |\n")
+    @RobotKeyword("Get the message that is displayed in the default status bar"
+      + " at the bottom of the screen. \n\n "
+      + "Example:\n "
+      + "| Get Status Message |\n")
     public String getStatusMessage() {
         return new StatusBarOperator().getMessage();
+    }
+
+    @RobotKeyword("Get all the messages that are displayed in all the status bars"
+      + " at the bottom of the screen. Choose which status bars are used by index.\n\n "
+      + "Example:\n "
+      + "| Get All Status Messages | 0 | 1\n")
+    @ArgumentNames({ "*statusBarIndexes" })
+    public List<List <String>> getAllStatusMessages(String... statusBarIndexes) {
+        // Robot Framework convert arguments to strings, so don't use ints here
+        List<List <String>> allMessages = new ArrayList<>();
+        for (String index: statusBarIndexes) {
+          int i = Integer.parseInt(index);
+          List <String> messages = new StatusBarOperator(i).getAllMessages();
+          allMessages.add(messages);
+        }
+        return allMessages;
     }
 
     @RobotKeyword("Verify that the status bar at the bottom of the screen contains certain content.\n\n" + "Example:\n"
--- src/main/java/org/robotframework/formslibrary/operator/StatusBarOperator.java
+++ src/main/java/org/robotframework/formslibrary/operator/StatusBarOperator.java
@@ -1,5 +1,8 @@
 package org.robotframework.formslibrary.operator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.robotframework.formslibrary.FormsLibraryException;
 import org.robotframework.formslibrary.chooser.ByComponentTypeChooser;
 import org.robotframework.formslibrary.util.ComponentType;
@@ -8,10 +11,9 @@ import org.robotframework.formslibrary.util.ObjectUtil;
 import org.robotframework.formslibrary.util.TextUtil;
 
 /**
- * Oracle Forms default Status Bar operator.
+ * Oracle Forms Status Bar operator.
  */
 public class StatusBarOperator extends AbstractRootComponentOperator {
-
     /**
      * Create a new operator for the default status bar.
      */
@@ -20,10 +22,20 @@ public class StatusBarOperator extends AbstractRootComponentOperator {
     }
 
     /**
+     * Create a new operator for the chosen status bar.
+     * Status bar is chosen by index, if the form contains
+     * multiple status bars.
+     */
+    public StatusBarOperator(int statusBarIndex) {
+      // TODO: make this work with statusBarIndex = -1
+      // which should return *all* status bars here
+        super(new ByComponentTypeChooser(statusBarIndex, ComponentType.STATUS_BAR));
+    }
+
+    /**
      * @return the main message displayed in the status bar.
      */
     public String getMessage() {
-
         Object[] statusBarItems = (Object[]) ObjectUtil.invokeMethod(getSource(), "getItems()");
         String result = "";
         for (int i = 0; i < statusBarItems.length; i++) {
@@ -34,6 +46,25 @@ public class StatusBarOperator extends AbstractRootComponentOperator {
             }
         }
         return result;
+    }
+
+    /**
+     * @return the all the messages displayed in the status bar.
+     */
+    public List<String> getAllMessages() {
+
+        Object[] statusBarItems = (Object[]) ObjectUtil.invokeMethod(getSource(), "getItems()");
+        List<String> results = new ArrayList<>();
+        String result = "";
+
+        for (int i = 0; i < statusBarItems.length; i++) {
+            if (ComponentType.STATUS_BAR_TEXT_ITEM.matches(statusBarItems[i])) {
+                result = ObjectUtil.getString(statusBarItems[i], "getText()");
+                Logger.info("Found status message '" + result + "'");
+                results.add(result);
+            }
+        }
+        return results;
     }
 
     /**
